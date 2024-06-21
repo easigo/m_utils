671fd8be627cea8348058f35feb1551b67a3acf3
mo7 <mo7@mo7.cc>
2024-06-21 15:24:14 +0800

    m_json方法

---
+++ m_json/json.go
@@ -0,0 +1,48 @@
+package m_json
+
+import (
+	"bytes"
+	"encoding/json"
+	"go-tools/m_str"
+
+	jsoniter "github.com/json-iterator/go"
+)
+
+// struct 转成 byte
+func ToJson(data any) []byte {
+	jsonStr, err := jsoniter.Marshal(data)
+	if err != nil {
+		return []byte{}
+	}
+	return jsonStr
+}
+
+// struct 转成  string
+func ToStr(data any) string {
+	byteStr := ToJson(data)
+	jsonStr := m_str.ToStr(byteStr)
+	return jsonStr
+}
+
+// byte 格式化 json 体
+func JsonFormat(jsonByte []byte) string {
+	var out bytes.Buffer
+	json.Indent(&out, jsonByte, "", " ")
+	return out.String()
+}
+
+// struct 格式化 json 体
+func Format(data any) string {
+	jsonByte := ToJson(data)
+	return JsonFormat(jsonByte)
+}
+
+// struct 转成 map
+func StructToMap(val any) (resData map[string]any) {
+	jsonByte := ToJson(val)
+	err := jsoniter.Unmarshal(jsonByte, &resData)
+	if err != nil {
+		resData = map[string]any{}
+	}
+	return
+}
