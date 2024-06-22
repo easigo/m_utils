a9d049d67ffe8151e210c6688ba0fc1cb73993ad
墨七 <mo7@mo7.cc>
2024-06-22 22:50:27 +0800

    新增DataMap的处理

--- m_fetch/httpClient.go
+++ m_fetch/httpClient.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gocolly/colly/v2"
 	"github.com/gocolly/colly/v2/proxy"
+	"github.com/handy-golang/go-tools/m_json"
 	"github.com/handy-golang/go-tools/m_str"
 	"github.com/handy-golang/go-tools/m_url"
 	jsoniter "github.com/json-iterator/go"
@@ -42,6 +43,7 @@ type HttpOpt struct {
 	Origin    string
 	Path      string
 	Data      []byte
+	DataMap   map[string]any // 如果存在 DataMap 则会忽略  Data 参数内容
 	Header    map[string]string
 	Event     func(string, any) // s1 = succeed , err
 	ProxyURLs []string          //  ["socks5://127.0.0.1:1337", "socks5://127.0.0.1:1338"]
@@ -72,15 +74,20 @@ func NewHttp(opt HttpOpt) (_this *Http) {
 	}
 
 	_this.Url = m_str.Join(opt.Origin, opt.Path)
+
 	_this.Data = opt.Data
+	if len(opt.DataMap) > 0 {
+		_this.Data = m_json.ToJson(opt.DataMap)
+	}
+
 	_this.Header = opt.Header
 	_this.Event = opt.Event
 	if _this.Event == nil {
 		_this.Event = func(s1 string, s2 any) {}
 	}
 
+	// 创建 colly 对象
 	_this.C = colly.NewCollector()
-
 	if len(opt.ProxyURLs) > 0 {
 		// 设置代理
 		rp, err := proxy.RoundRobinProxySwitcher(
@@ -92,12 +99,20 @@ func NewHttp(opt HttpOpt) (_this *Http) {
 		}
 		_this.C.SetProxyFunc(rp)
 	}
+	_this.C.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("Content-Type", "application/json; charset=utf-8")
+		r.Headers.Set("User-Agent", "go-tools - github.com/handy-golang/go-tools")
+		// 添加header头
+		for key, val := range _this.Header {
+			r.Headers.Set(key, val)
+		}
+	})
 
 	return
 }
 
 // 处理 Get 参数
-func (_this *Http) DisposeGetParam() *Http {
+func (_this *Http) disposeGetParam() *Http {
 	urlO := m_url.InitUrl(_this.Url)
 
 	var dataUn map[string]any
@@ -113,17 +128,8 @@ func (_this *Http) DisposeGetParam() *Http {
 
 // GET
 func (_this *Http) Get() (resData []byte, resErr error) {
-	// 处理参数请求
-	_this.DisposeGetParam()
-
-	_this.C.OnRequest(func(r *colly.Request) {
-		r.Headers.Set("Content-Type", "application/json; charset=utf-8")
-		r.Headers.Set("User-Agent", "goTools - github.com/EasyGolang/goTools")
-		// 添加header头
-		for key, val := range _this.Header {
-			r.Headers.Set(key, val)
-		}
-	})
+	// 处理 Get 参数
+	_this.disposeGetParam()
 
 	_this.C.OnResponse(func(r *colly.Response) {
 		resData = r.Body
@@ -141,17 +147,7 @@ func (_this *Http) Get() (resData []byte, resErr error) {
 }
 
 // Post
-
 func (_this *Http) Post() (resData []byte, resErr error) {
-
-	_this.C.OnRequest(func(r *colly.Request) {
-		r.Headers.Set("Content-Type", "application/json; charset=utf-8")
-		r.Headers.Set("User-Agent", "goTools - github.com/EasyGolang/goTools")
-		// 添加header头
-		for key, val := range _this.Header {
-			r.Headers.Set(key, val)
-		}
-	})
 
 	_this.C.OnResponse(func(r *colly.Response) {
 		resData = r.Body
--- m_json/json.go
+++ m_json/json.go
@@ -8,7 +8,7 @@ import (
 	jsoniter "github.com/json-iterator/go"
 )
 
-// struct 转成 byte
+// struct 转成 json byte
 func ToJson(data any) []byte {
 	jsonStr, err := jsoniter.Marshal(data)
 	if err != nil {
@@ -17,21 +17,21 @@ func ToJson(data any) []byte {
 	return jsonStr
 }
 
-// struct 转成  string
+// struct 转成 json string
 func ToStr(data any) string {
 	byteStr := ToJson(data)
 	jsonStr := m_str.ToStr(byteStr)
 	return jsonStr
 }
 
-// byte 格式化成 json 体
+// json byte 转成 json string 并格式化
 func JsonFormat(jsonByte []byte) string {
 	var out bytes.Buffer
 	json.Indent(&out, jsonByte, "", " ")
 	return out.String()
 }
 
-// struct 格式化 json 体
+// struct 转成 json string 并格式化
 func Format(data any) string {
 	jsonByte := ToJson(data)
 	return JsonFormat(jsonByte)
